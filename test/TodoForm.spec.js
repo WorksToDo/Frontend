9621ef45f329bd4ae1a0b936c01720ec9879f028
azumber <ahmet.zumberoglu@gmail.com>
2022-01-26 22:02:53 +0300

    [02][Ahmet/Enes] TDD has began
    TodoForm component tests has been failed.
    TDD process has running correctly.

---
+++ test/TodoForm.spec.js
@@ -0,0 +1,34 @@
+import { mount } from '@vue/test-utils'
+import TodoForm from '~/components/TodoForm.vue'
+
+describe('TodoForm', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(TodoForm)
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
+
+describe('TodoForm', () => {
+  test('is todo form valid', async () => {
+    const wrapper = mount(TodoForm)
+    const input = await wrapper.find('#todo-input')
+    input.setValue('buy some milk')
+    expect(input).toBeTruthy()
+    expect(wrapper.vm.input).toBe('buy some milk')
+  })
+})
+
+describe('TodoForm', () => {
+  test('is save button clickable', async () => {
+    const wrapper = mount(TodoForm)
+    const mockSave = jest.fn()
+
+    wrapper.setMethods({
+      saveTodo: mockSave
+    })
+
+    const button = wrapper.find('#save')
+    await button.trigger('click')
+    expect(mockSave).toHaveBeenCalled()
+  })
+})
